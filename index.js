7b2ae6d9c49357e03c538502e269599009201d39
ianrich69420 <ian.orieko@student.moringaschool.com>
2022-11-23 16:58:25 +0300

    Completed assignment
    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(stringNames, eventName) {
+  const newStringNames = [];
+  for (let i = 0; i < stringNames.length; i++) {
+    newStringNames.splice(
+      i,
+      0,
+      `Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return newStringNames;
+}
+
+function countDown(positiveInt) {
+  let i = positiveInt;
+  while (i > -1) {
+    console.log(i);
+    --i;
+  }
+}
